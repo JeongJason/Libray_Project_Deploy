7a8e154c7f65eb40e39560e296dc98175ad1cb8e
Jaewon <jewon15@gmail.com>
2023-10-27 11:29:03 +0900

    deploy

--- src/main/java/com/example/app/auth/loginHandler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/example/app/auth/loginHandler/CustomAuthenticationFailureHandler.java
@@ -1,5 +1,7 @@
 package com.example.app.auth.loginHandler;
 
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,22 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
-
+    private final String DEFAULT_FAILURE_URL = "login?error=true";
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
         System.out.println("로그인 실패! MSG : " + exception.getMessage());
 
-        response.sendRedirect("login");
+        String loginFailMsg = null;
 
-//        response.sendRedirect(request.getContextPath()+"/login?error="+exception.getMessage());
+        if (exception instanceof AuthenticationServiceException) {
+            loginFailMsg = "존재하지 않는 사용자입니다.";
+        } else if(exception instanceof BadCredentialsException) {
+            loginFailMsg = "아이디 또는 비밀번호가 틀립니다.";
+        }
+
+        request.setAttribute("loginFailMsg", loginFailMsg);
+        request.getRequestDispatcher(DEFAULT_FAILURE_URL).forward(request,response);
+
     }
 }
