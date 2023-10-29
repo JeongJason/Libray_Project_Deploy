9f17a2e003db68d78ff5a7bf8fc5fba30dfa3b09
Jaewon <jewon15@gmail.com>
2023-10-29 23:04:53 +0900

    deploy

--- src/main/java/com/example/app/mapper/ReviewMapper.java
+++ src/main/java/com/example/app/mapper/ReviewMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface ReviewMapper {
 
     // 리뷰 조회
-    public ReviewDTO select(Long revId);
+    public ReviewDTO selectreview(Long revId);
     // 리뷰 목록
     public List<ReviewDTO> selectAll(@Param("cri")Criteria criteria, @Param("search")Search search);
     // 리뷰 추가
